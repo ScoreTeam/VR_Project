1b85c65477ac6ca0b511de022ad3828e6dc581dd
Hamza MB <hamzamuhareb777@gmail.com>
2024-06-03 23:57:26 +0300

    added particles generator

--- Assets/Scripts/Sim 3D/Simulation3D.cs
+++ Assets/Scripts/Sim 3D/Simulation3D.cs
@@ -17,6 +17,10 @@ public class Simulation3D : MonoBehaviour
     public float nearPressureMultiplier;
     public float viscosityStrength;
 
+    private static Vector3[] obstacleCentres;
+    private static Vector3[] obstacleSizes;
+
+
     [Header("References")]
     public ComputeShader compute;
     public Spawner3D spawner;
@@ -25,7 +29,9 @@ public class Simulation3D : MonoBehaviour
 
     // Buffers
     public ComputeBuffer positionBuffer { get; private set; }
+    public ComputeBuffer initPositionBuffer { get; private set; }
     public ComputeBuffer velocityBuffer { get; private set; }
+    public ComputeBuffer initVelocityBuffer { get; private set; }
     public ComputeBuffer densityBuffer { get; private set; }
     public ComputeBuffer predictedPositionsBuffer;
     ComputeBuffer spatialIndices;
@@ -44,6 +50,7 @@ public class Simulation3D : MonoBehaviour
     const int pressureKernel = 3;
     const int viscosityKernel = 4;
     const int updatePositionsKernel = 5;
+    const int collisionDetection = 6;
 
     GPUSort gpuSort;
 
@@ -52,6 +59,18 @@ public class Simulation3D : MonoBehaviour
     bool pauseNextFrame;
     Spawner3D.SpawnData spawnData;
 
+
+    static Simulation3D()
+    {
+        // Initialize obstacle centers and sizes
+        obstacleCentres = new Vector3[4];
+        obstacleSizes = new Vector3[4];
+        for (int i = 0; i < 4; i++)
+        {
+            obstacleCentres[i] = new Vector3(i * 3 - 4.5f, i - 1.5f, 0);
+            obstacleSizes[i] = new Vector3(1, 1, 5);
+        }
+    }
     void Start()
     {
         Debug.Log("Controls: Space = Play/Pause, R = Reset");
@@ -65,28 +84,37 @@ public class Simulation3D : MonoBehaviour
         // Create buffers
         int numParticles = spawnData.points.Length;
         positionBuffer = ComputeHelper.CreateStructuredBuffer<float3>(numParticles);
+        initPositionBuffer = ComputeHelper.CreateStructuredBuffer<float3>(numParticles);
         predictedPositionsBuffer = ComputeHelper.CreateStructuredBuffer<float3>(numParticles);
         velocityBuffer = ComputeHelper.CreateStructuredBuffer<float3>(numParticles);
+        initVelocityBuffer = ComputeHelper.CreateStructuredBuffer<float3>(numParticles);
         densityBuffer = ComputeHelper.CreateStructuredBuffer<float2>(numParticles);
         spatialIndices = ComputeHelper.CreateStructuredBuffer<uint3>(numParticles);
         spatialOffsets = ComputeHelper.CreateStructuredBuffer<uint>(numParticles);
+
+        ObBoxesCenters = ComputeHelper.CreateStructuredBuffer<float3>(1000);
+        ObBoxesSizes = ComputeHelper.CreateStructuredBuffer<float3>(1000);
 
         // Set buffer data
         SetInitialBufferData(spawnData);
 
         // Init compute
         ComputeHelper.SetBuffer(compute, positionBuffer, "Positions", externalForcesKernel, updatePositionsKernel);
+        ComputeHelper.SetBuffer(compute, initPositionBuffer, "InitPositions", externalForcesKernel, updatePositionsKernel);
         ComputeHelper.SetBuffer(compute, predictedPositionsBuffer, "PredictedPositions", externalForcesKernel, spatialHashKernel, densityKernel, pressureKernel, viscosityKernel, updatePositionsKernel);
         ComputeHelper.SetBuffer(compute, spatialIndices, "SpatialIndices", spatialHashKernel, densityKernel, pressureKernel, viscosityKernel);
         ComputeHelper.SetBuffer(compute, spatialOffsets, "SpatialOffsets", spatialHashKernel, densityKernel, pressureKernel, viscosityKernel);
         ComputeHelper.SetBuffer(compute, densityBuffer, "Densities", densityKernel, pressureKernel, viscosityKernel);
         ComputeHelper.SetBuffer(compute, velocityBuffer, "Velocities", externalForcesKernel, pressureKernel, viscosityKernel, updatePositionsKernel);
+        ComputeHelper.SetBuffer(compute, initVelocityBuffer, "InitVelocities", externalForcesKernel, pressureKernel, viscosityKernel, updatePositionsKernel);
+
+        ComputeHelper.SetBuffer(compute, ObBoxesCenters, "ObCenters", collisionDetection, viscosityKernel, updatePositionsKernel);
+        ComputeHelper.SetBuffer(compute, ObBoxesSizes, "ObSizes", collisionDetection, viscosityKernel, updatePositionsKernel);
 
         compute.SetInt("numParticles", positionBuffer.count);
 
         gpuSort = new();
         gpuSort.SetBuffers(spatialIndices, spatialOffsets);
-
 
         // Init display
         display.Init(this);
@@ -145,6 +173,7 @@ public class Simulation3D : MonoBehaviour
         ComputeHelper.Dispatch(compute, positionBuffer.count, kernelIndex: pressureKernel);
         ComputeHelper.Dispatch(compute, positionBuffer.count, kernelIndex: viscosityKernel);
         ComputeHelper.Dispatch(compute, positionBuffer.count, kernelIndex: updatePositionsKernel);
+        // ComputeHelper.Dispatch(compute, positionBuffer.count, kernelIndex: collisionDetection);
 
     }
 
@@ -164,6 +193,10 @@ public class Simulation3D : MonoBehaviour
         compute.SetVector("boundsSize", BoundsSize);
         compute.SetVector("centre", BoundsCentre);
 
+        compute.SetInt("numObstacles", obstacleCentres.Length);
+        compute.SetVectorArray("obstacleSizes", ConvertToVector4Array(obstacleSizes));
+        compute.SetVectorArray("obstacleCentres", ConvertToVector4Array(obstacleCentres));
+
         compute.SetMatrix("localToWorld", transform.localToWorldMatrix);
         compute.SetMatrix("worldToLocal", transform.worldToLocalMatrix);
     }
@@ -174,8 +207,10 @@ public class Simulation3D : MonoBehaviour
         System.Array.Copy(spawnData.points, allPoints, spawnData.points.Length);
 
         positionBuffer.SetData(allPoints);
+        initPositionBuffer.SetData(allPoints);
         predictedPositionsBuffer.SetData(allPoints);
         velocityBuffer.SetData(spawnData.velocities);
+        initVelocityBuffer.SetData(spawnData.velocities);
     }
 
     void HandleInput()
@@ -200,21 +235,74 @@ public class Simulation3D : MonoBehaviour
 
     void OnDestroy()
     {
-        ComputeHelper.Release(positionBuffer, predictedPositionsBuffer, velocityBuffer, densityBuffer, spatialIndices, spatialOffsets);
+        ComputeHelper.Release(positionBuffer, predictedPositionsBuffer, velocityBuffer, densityBuffer, spatialIndices, spatialOffsets, ObBoxesCenters, ObBoxesSizes);
     }
 
     void OnDrawGizmos()
     {
         // Draw Bounds
-        
+
         floorDisplay.transform.localScale = new Vector3(BoundsSize.x, 1 / BoundsSize.y * 0.1f, BoundsSize.z);
-        floorDisplay.transform.position = new Vector3(BoundsCentre.x,BoundsCentre.y - BoundsSize.y*0.5f,BoundsCentre.z);
-        
+        floorDisplay.transform.position = new Vector3(BoundsCentre.x, BoundsCentre.y - BoundsSize.y * 0.5f, BoundsCentre.z);
+
         var m = Gizmos.matrix;
         Gizmos.matrix = transform.localToWorldMatrix;
         Gizmos.color = new Color(0, 1, 0, 0.5f);
         Gizmos.DrawWireCube(BoundsCentre, BoundsSize);
+        for (int i = 0; i < obstacleCentres.Length; i++)
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            Gizmos.DrawWireCube(obstacleCentres[i], obstacleSizes[i]);
+        }
         Gizmos.matrix = m;
 
     }
+
+    Vector4[] ConvertToVector4Array(Vector3[] input)
+    {
+        Vector4[] output = new Vector4[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            output[i] = input[i];
+        }
+        return output;
+    }
+    // public static void ReGenerateParticles(ComputeBuffer PositionsBuffer, ComputeBuffer VelocitiesBuffer, int ParticlesLayer, Vector3 centre, float size, float3 initialVel, float jitterStrength)
+    //     {
+    //         Vector3[] positionsData = new Vector3[PositionsBuffer.count];
+    //         Vector3[] velocitiesData = new Vector3[VelocitiesBuffer.count];
+    //         PositionsBuffer.GetData(positionsData);
+    //         VelocitiesBuffer.GetData(velocitiesData);
+    //         int n = Convert.ToInt32(Math.Pow(PositionsBuffer.count, 1f / 3f));
+
+    //         int particleIndex = n * (ParticlesLayer - 1);
+
+    //         // Vector3 posLocal = WorldToLocal.MultiplyPoint(positionsData[particleIndex]);
+    //         // Vector3 velocityLocal = WorldToLocal.MultiplyVector(velocitiesData[particleIndex]);
+    //         for (int y = 0; y < n; y++)
+    //         {
+    //             for (int z = 0; z < n; z++)
+    //             {
+    //                 float tx = 0 / (n - 1f);
+    //                 float ty = y / (n - 1f);
+    //                 float tz = z / (n - 1f);
+
+    //                 float px = (tx - 0.5f) * size + centre.x;
+    //                 float py = (ty - 0.5f) * size + centre.y;
+    //                 float pz = (tz - 0.5f) * size + centre.z;
+    //                 float3 jitter = UnityEngine.Random.insideUnitSphere * jitterStrength;
+    //                 positionsData[particleIndex] = new float3(px, py, pz) + jitter;
+    //                 velocitiesData[particleIndex] = initialVel;
+    //                 particleIndex++;
+    //             }
+    //             // positionsData[particleIndex] = LocalToWorld.MultiplyPoint(posLocal);
+    //             // velocitiesData[particleIndex] = LocalToWorld.MultiplyPoint(velocityLocal);
+    //         }
+    //         // Set the modified data back to the buffer
+    //         PositionsBuffer.SetData(positionsData);
+    //         VelocitiesBuffer.SetData(velocitiesData);
+    //     }
+
+
+
 }
